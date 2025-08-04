7a1706ebab559539e35d640859a1d0de7dc5a2bc
c3-CarlosCardenasRuiz <118222289+c3-CarlosCardenasRuiz@users.noreply.github.com>
2025-08-04 12:30:51 -0600

    Update package_specific.sh

--- package_specific.sh
+++ package_specific.sh
@@ -2,24 +2,6 @@
 
 set -eu -o pipefail
 
-SCRIPT_DIR=${BASH_SOURCE%/*}
-
-if [[ $REPO == "facebookresearch/pytorch3d" ]] && [[ $REPO == "facebookresearch/pytorch3d" ]] \
-  && { [[ $COMPUTE_PLATFORM == "cu118" ]] || [[ $COMPUTE_PLATFORM == "cu121" ]]; }; then
-  CUB_VERSION="1.17.2"
-  mkdir cub
-  curl -L https://github.com/NVIDIA/cub/archive/${CUB_VERSION}.tar.gz | tar -xzf - --strip-components=1 --directory cub
-  echo "CUB_HOME=$PWD/cub" >> "$GITHUB_ENV"
-fi
-
-if [[ $REPO == "facebookresearch/fairseq" ]]; then
-  pip install cython
-  patch -p0 < "$SCRIPT_DIR"/package_specific/fairseq_cub.patch
-fi
-
-if [[ $REPO == "NVlabs/tiny-cuda-nn" ]]; then
-  source "$SCRIPT_DIR"/.github/workflows/cuda/${OS}_env.sh
-  echo "LIBRARY_PATH=/usr/local/cuda/lib64/stubs" >> "$GITHUB_ENV"
-  echo "TCNN_CUDA_ARCHITECTURES=${TORCH_CUDA_ARCH_LIST}" | sed "s/\(\.\|\+PTX\)//g" >> "$GITHUB_ENV"
-  patch -p0 < "$SCRIPT_DIR"/package_specific/tiny_cuda_nn.patch
-fi
+PACKAGE_NAME=detectron2
+REPO=facebookresearch/detectron2
+COMMIT_OR_TAG=v0.6
